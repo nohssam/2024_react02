a8e0df451373c7d0d58914abebd94cea20e480c2
nohssam <nojm73@naver.com>
2024-11-21 17:55:33 +0900

    Callback 예제

---
+++ src/components/step10/CallBackTest02.jsx
@@ -0,0 +1,44 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+function CallBackTest02(props) {
+    const [number, setNumber] = useState(0);
+
+    // useEffect에서 input에서 증가/감소 클릭할 때마다 리렌더링이되면 
+    // someFunction 의 객체(함수) 새로 생성 되면거 변경된것을 감지해서 
+    // const someFunction = () => {
+    //     console.log(`someFunc : number : ${number}`);
+    //     return;
+    // }
+
+
+    // useCallback은 함수를 자체를 저장한다.
+    // 버튼을 누르면 someFunc : number : 0 나오다 이유는 useCallback 저장할때 당시 number0이었다.
+    // const someFunction = useCallback(() => {
+    //     console.log(`someFunc : number : ${number}`);
+    //     return;
+    // }, []);
+
+
+    const someFunction = useCallback(() => {
+        console.log(`someFunc : number : ${number}`);
+        return;
+    }, [number]);
+
+    useEffect(() => {
+        console.log("someFunc 변경 되었네요")
+    }, [someFunction]);
+
+    return (
+        <div>
+            <input
+                type='number'
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+            />
+            <br />
+            <button onClick={someFunction}>someFunction 함수 호출</button>
+        </div>
+    );
+}
+
+export default CallBackTest02;
--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ import MemoTest01 from './components/step09/MemoTest01';
 import MemoTest02 from './components/step09/MemoTest02';
 import MemoTest03 from './components/step09/MemoTest03';
 import MemoTest04 from './components/step09/MemoTest04';
+import CallBackTest01 from './components/step10/CallBackTest01';
+import CallBackTest02 from './components/step10/CallBackTest02';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -43,7 +45,9 @@ root.render(
   // <MemoTest01 />
   // <MemoTest02 />
   // <MemoTest03 />
-  <MemoTest04 />
+  // <MemoTest04 />
+  // <CallBackTest01 />
+  <CallBackTest02 />
 
 );
 reportWebVitals();
